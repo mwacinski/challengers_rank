06ee628c5b8daf8dda64b38a3b0f39e26a6a25aa
Michał Waciński <mrfreezy48@gmail.com>
2023-01-04 11:55:10 +0100

    update

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     URL = "https://euw1.api.riotgames.com/lol/league/v4/" \
           "challengerleagues/by-queue/RANKED_SOLO_5x5?api_key="
 
-    API_KEY = ""
+    API_KEY = "RGAPI-aedd4a1c-c08d-43ff-ae15-6a00f3c3ee6f"
 
     FILE_PATH = "data.csv"
